954270fa5509022b6175a0dce807582991a5cddb
shanky-ced <shashankrai2022jun@cedcoss.com>
2022-08-04 12:16:35 +0530

    Password function added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // State to hold password
+  const [password, setPassword] = useState("");
+  var passw="";
+
+  const genrate = (e) => {
+    e.preventDefault();
+    
+    const smallLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const number = ["0", "1", "2", "3", "4","5", "6", "7", "8", "9","@"];
+
+    // Loop to genrate random index for password
+    for(let i=0;i<3;i++){
+      passw += smallLetter[Math.floor(1+Math.random()*25)];
+      passw+=smallLetter[Math.floor(1+Math.random()*25)].toUpperCase();
+      passw+=number[Math.floor(1+Math.random()*9)];
+     
+    }
+    
+    document.querySelector("#pass").innerHTML = `Password is : ${passw}`;
+  
+
+  }
+  const copy = (e) => {
+    e.preventDefault();
+    setPassword(passw);
+    alert( `Your password (${passw}) is copied`);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <button onClick={genrate}>Click to Genrate Password</button>
+        <p id='pass'></p>
+        <button onClick={copy}>Click to Copy Password</button>
+      </div>
+    </>
   );
 }
 
