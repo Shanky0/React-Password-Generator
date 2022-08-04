2de3cac3e7740f752b06c00dee10bc1ca36944de
shanky-ced <shashankrai2022jun@cedcoss.com>
2022-08-04 12:20:24 +0530

    Condition added

--- src/App.js
+++ src/App.js
@@ -6,30 +6,40 @@ function App() {
 
   // State to hold password
   const [password, setPassword] = useState("");
-  var passw="";
+  var passw = "";
 
   const genrate = (e) => {
     e.preventDefault();
-    
+
+    passw= "";
     const smallLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const number = ["0", "1", "2", "3", "4","5", "6", "7", "8", "9","@"];
+    const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "@"];
+
 
     // Loop to genrate random index for password
-    for(let i=0;i<3;i++){
-      passw += smallLetter[Math.floor(1+Math.random()*25)];
-      passw+=smallLetter[Math.floor(1+Math.random()*25)].toUpperCase();
-      passw+=number[Math.floor(1+Math.random()*9)];
-     
+    for (let i = 0; i < 3; i++) {
+      passw += smallLetter[Math.floor(1 + Math.random() * 25)];
+      passw += smallLetter[Math.floor(1 + Math.random() * 25)].toUpperCase();
+      passw += number[Math.floor(1 + Math.random() * 9)];
+
     }
-    
+
     document.querySelector("#pass").innerHTML = `Password is : ${passw}`;
-  
+    document.querySelector("#pass").style="color:green";
+
 
   }
   const copy = (e) => {
     e.preventDefault();
-    setPassword(passw);
-    alert( `Your password (${passw}) is copied`);
+
+    if (passw === "") {
+      document.querySelector("#pass").innerHTML = "Genrate password first";
+      document.querySelector("#pass").style="color:red";
+    } else {
+      setPassword(passw);
+      alert(`Your password (${passw}) is copied`);
+    }
+
 
   }
   return (
